60cc4e84d22408620eef5adae2c5f587d6428481
Christopher J <sharkchris91@gmail.com>
2023-08-31 18:19:40 +0100

    fix: non-working thing and we might want to change library

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 //from: https://www.digitalocean.com/community/tutorials/typescript-new-project
 
+import path from "path";
+import { extractData } from "./qrDataExtractor";
+
 const world = 'world';
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
-}
 
-console.log(hello(world))
+const pdfFilePath = path.resolve(__dirname, './sample.pdf');
+
+extractData(pdfFilePath)
